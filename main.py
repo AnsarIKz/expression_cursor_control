e2fe0e64f79287a2bc754cb2c7a86a95baf948bf
Ansar Nurzhanov <nansarik@mail.ru>
2024-01-04 17:25:58 +0600

    feet: def detect_blink

--- main.py
+++ main.py
@@ -23,8 +23,36 @@ def get_horizontal_tilt(left_eye, right_eye):
     angle_radians = (angle_radians + 2 * np.pi) % (2 * np.pi)
     return math.degrees(angle_radians) * -1
 
-def get_vertical_tilt(nose_nasal, nose_septum):
-    return nose_septum[1] - nose_nasal[1]
+def get_vertical_tilt(btm, top):
+    return btm[1] - top[1]
+
+def detect_blink(eye_open_threshold, landmarks):
+    prev_left_eye_open = True
+    prev_right_eye_open = True
+    if eye_open_threshold is None:
+        pass
+    else:
+        # Рассчитаем вертикальное отклонение глаз
+        left_eye_vertical_tilt = get_vertical_tilt(np.array([landmarks.part(46).x, landmarks.part(46).y]), np.array([landmarks.part(44).x, landmarks.part(44).y]))
+        right_eye_vertical_tilt = get_vertical_tilt(np.array([landmarks.part(41).x, landmarks.part(41).y]), np.array([landmarks.part(37).x, landmarks.part(37).y]))
+
+        # Определение открытости глаза
+        left_eye_open = abs(left_eye_vertical_tilt) < eye_open_threshold
+        right_eye_open = abs(right_eye_vertical_tilt) < eye_open_threshold
+
+        # Детекция моргания
+        if prev_left_eye_open and not left_eye_open:
+            # pyautogui.mouseDown(button='left')
+            pass
+        elif left_eye_open:
+            pyautogui.mouseUp(button='left')
+        if prev_right_eye_open and not right_eye_open:
+            print("Правый глаз закрыт - Моргание")
+
+        # Обновление предыдущего состояния глаз
+        prev_left_eye_open = left_eye_open
+        prev_right_eye_open = right_eye_open
+        # return left_eye_open, right_eye_open, left_eye_vertical_tilt, right_eye_vertical_tilt
 
 
 def process_face(frame, landmarks, calibration_points, screen_width, screen_height):
@@ -37,6 +65,8 @@ def process_face(frame, landmarks, calibration_points, screen_width, screen_heig
     horizontal_tilt = get_horizontal_tilt(left_eye, right_eye)
     vertical_tilt = get_vertical_tilt((landmarks.part(30).x, landmarks.part(30).y),
                                       (landmarks.part(27).x, landmarks.part(27).y))
+
+    detect_blink(eye_open_threshold=calibration_points['eye_threshold'], landmarks=landmarks)
     return (left_eye, right_eye, horizontal_tilt, vertical_tilt)
 
 def display_text(frame, text, x, y):
@@ -57,8 +87,8 @@ def move_mouse(horizontal_tilt, vertical_tilt, calibration_points, screen_width,
     pyautogui.moveTo(x_movement, y_movement, 0.2)
 
 def face_calibration():
-    calibration_points = {'left': None, 'right': None, 'up': None, 'down': None}
-    calibration_directions = ["left", "right", "up", "down"]
+    calibration_points = {'left': None, 'right': None, 'up': None, 'down': None, 'eye_threshold': None}
+    calibration_directions = ["left", "right", "up", "down", "eye_threshold"]
     calibration_direction = calibration_directions[0]
     while not all(calibration_points.values()):
         ret, frame = cap.read()
